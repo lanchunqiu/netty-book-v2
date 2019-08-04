c58e25733b373529f4d1847039c369e2cf5426ac
1046582540@qq.com <1046582540@qq.com>
2019-08-04 23:33:33 +0800

    jkd序列化的性能分析

---
+++ src/main/java/com/lancq/netty/codec/serializable/UserInfo.java
@@ -0,0 +1,66 @@
+package com.lancq.netty.codec.serializable;
+
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
+/**
+ * @author lancq
+ * @date 2019/8/4
+ **/
+public class UserInfo implements Serializable {
+    private String userName;
+
+    private int userID;
+
+    public UserInfo buildUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+
+    public UserInfo buildUserID(int userID) {
+        this.userID = userID;
+        return this;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public byte[] codeC() {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        byte[] value = this.userName.getBytes();
+        buffer.putInt(value.length);
+        buffer.put(value);
+        buffer.putInt(this.userID);
+        buffer.flip();
+        value = null;
+        byte[] result = new byte[buffer.remaining()];
+        buffer.get(result);
+        return result;
+    }
+
+    public byte[] codeC(ByteBuffer buffer) {
+        buffer.clear();
+        byte[] value = this.userName.getBytes();
+        buffer.putInt(value.length);
+        buffer.put(value);
+        buffer.putInt(this.userID);
+        buffer.flip();
+        value = null;
+        byte[] result = new byte[buffer.remaining()];
+        buffer.get(result);
+        return result;
+    }
+}
